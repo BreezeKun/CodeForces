ff03f6cee796818ab3425c47ec726e1e46cddf51
BreezeKun <akshadsatpute1989@gmail.com>
2026-08-13 20:08:31 +0530

    solved

---
+++ 800/ASoldierAndBananas.py
@@ -0,0 +1,37 @@
+# A. Soldier and Bananas
+# time limit per test1 second
+# memory limit per test256 megabytes
+# A soldier wants to buy w bananas in the shop. He has to pay k dollars for the first banana, 2k dollars for the second one and so on (in other words, he has to pay i·k dollars for the i-th banana).
+
+# He has n dollars. How many dollars does he have to borrow from his friend soldier to buy w bananas?
+
+# Input
+# The first line contains three positive integers k, n, w (1  ≤  k, w  ≤  1000, 0 ≤ n ≤ 109), the cost of the first banana, initial number of dollars the soldier has and number of bananas he wants.
+
+# Output
+# Output one integer — the amount of dollars that the soldier must borrow from his friend. If he doesn't have to borrow money, output 0.
+
+# Examples
+# InputCopy
+# 3 17 4
+# OutputCopy
+# 13
+
+k, n, w = map(int, input().split())
+t = 0
+for i in range (1,w+1):
+    t += i*k
+
+if n > t:
+    print(0)
+else:
+    print(t-n)
+
+
+#better approch
+
+k, n, w = map(int, input().split())
+
+total = sum(i * k for i in range(1, w + 1))
+
+print(max(0, total - n))
